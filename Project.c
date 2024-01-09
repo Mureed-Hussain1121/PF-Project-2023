f0d8fd74afce1a5003c00ae0f2fad5c1cd5aebb2
Mureed-Hussain1121 <hussainmureed313@gmail.com>
2024-01-09 13:27:33 +0500

    Addded Header files and structure

--- Project.c
+++ Project.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
 #include<conio.h>
+#include<string.h>
+#include<stdlib.h>
 
-int main()
-{
-    printf("HEllo world");
-
-
-    return 0;
-}
+struct account{
+	char name[40];
+	char dob[11];
+	char contact[15];
+	char email[40];
+	char city[15];
+	char address[60];
+	char CNIC[20];
+	char password[8];
+	char account_code[6] = "ACC-";
+	int account_number = 1000;
+	int balance;
+};
